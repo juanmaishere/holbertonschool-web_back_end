055d77e4570abe0542ec56da8fad5d7f6da4735c
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-02 23:11:25 +0000

    feat

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """sum list FUNCTION WIH ANOTATIONS"""
+from typing import List
 
 
-def sum_list(input_list: [float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Calculate the sum of a list of floats."""
     return sum(input_list)
