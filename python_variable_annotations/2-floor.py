3ec84121e0a1d7c4f763ec2e5c34c407930c4d13
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-02 19:47:02 +0000

    faet

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""floor function , returns the lowest int asociated with x float WIH ANOTATIONS"""
+from math import floor
+
+
+def floors(n: float) -> int:
+    """ Return mathfloor value for x float"""
+    res = floor(n)
+    return res
