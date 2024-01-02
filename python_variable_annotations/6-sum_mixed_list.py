d77119273a76bec159600c9f947053d1e0de927a
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-02 23:16:28 +0000

    faet

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum list FUNCTION WIH ANOTATIONS"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Calculate the sum of a list of floats."""
+    return sum(mxd_lst)
