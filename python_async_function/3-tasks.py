5607abf6f9cea2d32bd29499bdf33b36806bfe31
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-03 21:24:34 +0000

    feat

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Async co-routine"""
+import asyncio
+from asyncio import Task
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> Task:
+    """ Returns a task of waiting """
+    task1 = asyncio.create_task(wait_random(max_delay))
+    return task1
