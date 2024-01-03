b69f25d0217f948d8a8e47db7e6b59c0cb837961
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-03 21:52:48 +0000

    feat

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Module"""
+"""Async co-routine"""
 from typing import List
-task_wait_random = import('3-tasks').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Function to wait n number of times"""
-    i = 0
-    dic = []
-    for i in range(0, n):
-        result = await task_wait_random(max_delay)
-        dic.append(result)
-    return sorted(dic)
+    """ Wait n times and return a list of delays"""
+    lists = []
+    for _ in range(0, n):
+        delay = await task_wait_random(max_delay)
+        lists.append(delay)
+    sorted(lists)
+    return lists
