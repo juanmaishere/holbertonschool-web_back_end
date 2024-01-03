49d1e093859857e5ca38e42be2cd364261b5915f
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-03 20:21:50 +0000

    faet

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async co-routine"""
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    lists = []
+    for _ in range(0, n):
+        delay = await wait_random(max_delay)
+        lists.append(delay)
+    lists.sort()
+    return lists
