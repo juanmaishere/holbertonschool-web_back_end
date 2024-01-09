069cb366ba15b678c858f2362663325b79667cb2
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2024-01-09 17:04:13 -0300

    Update 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,8 +3,11 @@
 import pymongo
 
 def insert_school(mongo_collection, **kwargs):
+    """ Insert NEW DOCUMENT into mongodb collection """
     if mongo_collection:
         for key, value in kwargs.items():
             mongo_collection.insert_one({key: value})
+        id = mongo_collection.find("_id": )
+        return id
     else:
         return
