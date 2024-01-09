fc17ab197429ba2952e01f99c572bfbbbcd82311
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2024-01-09 17:01:25 -0300

    Update 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+""" MODULE COMMENT """
+import pymongo
 
+def insert_school(mongo_collection, **kwargs):
+    if mongo_collection:
+        for key, value in kwargs.items():
+            mongo_collection.insert_one({key: value})
+    else:
+        return
