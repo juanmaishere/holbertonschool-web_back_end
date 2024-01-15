ad3af7ffd3db643eb855cfd85accfd716be30926
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-15 20:51:50 +0000

    feat

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,7 @@
-const getSumOfHoods =
-  (Number, expansion1989 = 89, expansion2019 = 19) => Number + expansion1989 + expansion2019;
+const getSumOfHoods = (
+    Number,
+    expansion1989 = 89,
+    expansion2019 = 19
+  ) => Number + expansion1989 + expansion2019;
 
 export default getSumOfHoods;
