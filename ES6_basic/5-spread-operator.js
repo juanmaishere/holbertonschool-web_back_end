34a55eeb74203f99f18486c1054fc718eafa16bc
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-15 21:24:47 +0000

    feat

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,4 @@
-export default function concatArrays(...args) {
-  return [...args]
+export default function concatArrays(array1, array2, ...string) {
+  const concatenatedArray = [...array1, ...array2, ...(string.join(''))];
+  return concatenatedArray;
 }
