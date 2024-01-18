f7c576247be1dc2300057cc8fcf44ed6d402ef91
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-18 10:31:35 -0300

    feat

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom.
+ * @class
+ */
+export default class ClassRoom {
+    /**
+     * @constructor
+     * @param {number} maxStudentsSize - The maximum number of students allowed in the classroom.
+     */
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize
+    }
+}
