00b611eaaec04cd072a066187bf6bdae7c2796a0
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-21 14:35:52 -0300

    faet

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,28 +4,58 @@
  */
 export default class Currency {
 
-  constructor(name, code){
-    this._name = name;
-    this._code = code;
-  }
-
-  get name() {
-    return this._name
-  }
-  set name(name) {
-    if (typeof(name) === 'string') {
-      this._name = name
+    /**
+     * Constructor for the Currency class.
+     * @param {string} name - The name of the currency.
+     * @param {string} code - The code of the currency.
+     */
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    /**
+     * Get the name of the currency.
+     * @returns {string} - The name of the currency.
+     */
+    get name() {
+      return this._name;
+    }
+  
+    /**
+     * Set the name of the currency.
+     * @param {string} name - The new name for the currency.
+     */
+    set name(name) {
+      if (typeof(name) === 'string') {
+        this._name = name;
+      }
+    }
+  
+    /**
+     * Get the code of the currency.
+     * @returns {string} - The code of the currency.
+     */
+    get code() {
+      return this._code;
+    }
+  
+    /**
+     * Set the code of the currency.
+     * @param {string} code - The new code for the currency.
+     */
+    set code(code) {
+      if (typeof(code) === 'string') {
+        this._code = code;
+      }
+    }
+  
+    /**
+     * Display the full representation of the currency.
+     * @returns {string} - A formatted string containing the name and code of the currency.
+     */
+    displayFullCurrency() {
+      return `${this.name} (${this._code})`;
     }
   }
-  get code() {
-    return this._code
-  }
-  set code(code){
-    if (typeof(code) === 'string') {
-      this._code = code
-    }
-  }
-  displayFullCurrency(){
-  return `${this.name} (${this._code})`
-  }
-}
+  
