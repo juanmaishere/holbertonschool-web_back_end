afa55e6a699a53fdeb9b3ca482209e383a392e20
juanmaishere <rsjuanmanuel@hotmail.com>
2024-01-30 11:15:35 -0300

    feat

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
 export default function getListStudents(students) {
-  const ids = students()
-  if (ids === null && ids.length === 0) {
+  if (students === null && students.length === 0) {
     return []
   }
-  const id = ids.map((dicts) => (dicts.id))
-  return id
+  const ids = students.map((dicts) => (dicts.id))
+  return ids
 }
